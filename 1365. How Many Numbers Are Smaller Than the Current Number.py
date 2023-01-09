993bf703a8699afaa53c36efdc1400a83fa6bef3
Sagar Saha <59755509+TheSagarSaha@users.noreply.github.com>
2023-01-08 20:49:45 -0500

    Arrays
    Time Complexity: O(n*n)
    Space Complexity: O(n)

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        arr = []
+        for i in nums:
+            count = 0
+            for j in nums:
+                if j < i:
+                    count += 1
+            arr.append(count)
+        return arr
