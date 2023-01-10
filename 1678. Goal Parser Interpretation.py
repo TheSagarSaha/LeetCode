6073c444951107edfd5d331d22157d0b53cb893f
Sagar Saha <59755509+TheSagarSaha@users.noreply.github.com>
2023-01-10 16:26:51 -0500

    Strings
    Time Complexity: O(n)
    Space Complexity: O(n)

---
+++ 1678. Goal Parser Interpretation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        res = ""
+        i = 0
+        while i < len(command):
+            if command[i] == "G":
+                res += "G"
+                i += 1
+            elif command[i] == "(" and command[i+1] == ")":
+                res += "o"
+                i += 2
+            else:
+                res += "al"
+                i += 4
+        return res
